cbfc8d84627939666c5b944dc5b84914ab641c06
Teng Jun Siong <junsiong2599@gmail.com>
2024-04-28 00:13:06 +0800

    Removed boilerplate code from HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,6 @@
 import { useRouter } from 'vue-router'
 import { useAuthenticationStore } from '@/stores/authentication'
 import { useViewStore } from '@/stores/view'
-import SideBar from '@/components/nav/SideBar.vue'
 
 const router = useRouter()
 
@@ -31,24 +30,7 @@ const onLogoutClick = () => {
 </script>
 
 <template>
-  <!-- Layout wrapper -->
-  <div class="layout-wrapper layout-content-navbar">
-    <div class="layout-container">
-      <SideBar />
-      <div class="layout-page">
-        <div class="content-wrapper">
-          <div class="container-xxl flex-grow-1 container-p-y">
-            <h1>Welcome to the home page</h1>
-            <button @click.prevent="onLogoutClick" type="button" class="btn btn-primary">
-              Logout
-            </button>
-          </div>
-
-          <div class="content-backdrop fade"></div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <button @click.prevent="onLogoutClick" type="button" class="btn btn-primary">Logout</button>
 </template>
 
 <style scoped></style>
