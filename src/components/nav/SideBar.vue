a4ce7ce1cc2eae4b89763de859d520da5512f361
Teng Jun Siong <junsiong2599@gmail.com>
2024-04-28 00:11:07 +0800

    Updated HeaderBar and SideBar

--- src/components/nav/SideBar.vue
+++ src/components/nav/SideBar.vue
@@ -1,4 +1,55 @@
-<script setup></script>
+<script lang="ts" setup>
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+
+const router = useRouter()
+const route = useRoute()
+
+type NavItem = {
+  label: string
+  icon: string
+  routeName: string
+}
+
+const navItems: Ref<NavItem[]> = ref([
+  {
+    label: 'Sites',
+    icon: 'bx bx-sitemap',
+    routeName: 'Charge Point Locations'
+  },
+  {
+    label: 'Charge Points',
+    icon: 'bx bx-network-chart',
+    routeName: 'Charge Points'
+  },
+  {
+    label: 'Connectors',
+    icon: 'bx bx-plug',
+    routeName: 'Charge Point Ports'
+  },
+  {
+    label: 'User Groups',
+    icon: 'bx bx-group',
+    routeName: 'User Groups'
+  },
+  {
+    label: 'Users',
+    icon: 'bx bx-user',
+    routeName: 'Users'
+  }
+])
+
+const activeIndex = ref<number>(0)
+const onNavItemClick = (index: number): void => {
+  activeIndex.value = index
+  if (route.name != navItems.value[index].routeName) {
+    router.push({
+      name: navItems.value[index].routeName
+    })
+  }
+}
+</script>
 
 <template>
   <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
@@ -12,40 +63,15 @@
     </div>
 
     <ul class="menu-inner py-1">
-      <li class="menu-item">
+      <li
+        :class="['menu-item', { active: index == activeIndex }]"
+        v-for="(item, index) in navItems"
+        :key="index"
+        @click="onNavItemClick(index)"
+      >
         <div class="menu-link">
-          <i class="menu-icon bx bx-sitemap"></i>
-          <div>Sites</div>
-        </div>
-      </li>
-      <li class="menu-item">
-        <div class="menu-link">
-          <i class="menu-icon bx bx-network-chart"></i>
-          <div>Charge Points</div>
-        </div>
-      </li>
-      <li class="menu-item">
-        <div class="menu-link">
-          <i class="menu-icon bx bx-plug"></i>
-          <div>Connectors</div>
-        </div>
-      </li>
-      <li class="menu-item">
-        <div class="menu-link">
-          <i class="menu-icon bx bx-purchase-tag-alt"></i>
-          <div>Connector Prices</div>
-        </div>
-      </li>
-      <li class="menu-item active">
-        <div class="menu-link">
-          <i class="menu-icon bx bx-group"></i>
-          <div>User Groups</div>
-        </div>
-      </li>
-      <li class="menu-item">
-        <div class="menu-link">
-          <i class="menu-icon bx bx-user"></i>
-          <div>Users</div>
+          <i :class="['menu-icon', item.icon]"></i>
+          <div>{{ item.label }}</div>
         </div>
       </li>
     </ul>
