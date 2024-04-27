7f815fe82ab0ea2af932c5cc5e0b1b5970fa2d9a
Teng Jun Siong <junsiong2599@gmail.com>
2024-04-28 00:03:31 +0800

    Added DashboardLayout and DataTable

---
+++ src/components/tables/DataTable.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+interface Props {
+  title: string
+  headers: string[]
+  dataRows: Array<Array<string | number>>
+}
+
+withDefaults(defineProps<Props>(), {
+  title: '',
+  headers: () => [],
+  dataRows: () => []
+})
+</script>
+
+<template>
+  <div class="card">
+    <h5 class="card-header">{{ title }}</h5>
+    <div class="table-responsive text-nowrap">
+      <table class="table">
+        <thead>
+          <tr>
+            <th v-for="(header, index) in headers" :key="index">{{ header }}</th>
+          </tr>
+        </thead>
+        <tbody class="table-border-bottom-0">
+          <tr v-for="(row, index) in dataRows" :key="index">
+            <td v-for="(field, i) in row" :key="i">{{ field }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
--- src/views/layouts/DashboardLayout.vue
+++ src/views/layouts/DashboardLayout.vue
@@ -1,3 +1,20 @@
+<script lang="ts" setup>
+import SideBar from '@/components/nav/SideBar.vue'
+import HeaderBar from '@/components/header/HeaderBar.vue'
+</script>
+
 <template>
-  <RouterView />
+  <div class="layout-wrapper layout-content-navbar">
+    <div class="layout-container">
+      <div class="layout-page">
+        <HeaderBar />
+        <div class="content-wrapper">
+          <div class="container-xxl flex-grow-1 container-p-y">
+            <SideBar />
+            <RouterView />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
