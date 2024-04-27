e21dfcbbdbc33d33e881525c9331e53d2aa6744e
Teng Jun Siong <junsiong2599@gmail.com>
2024-04-28 00:13:41 +0800

    Updated router for new views

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { useAuthenticationStore } from '@/stores/authentication'
 
-import HomeView from '@/views/HomeView.vue'
-import LoginView from '@/views/LoginView.vue'
 import AuthLayout from '@/views/layouts/AuthLayout.vue'
 import DashboardLayout from '@/views/layouts/DashboardLayout.vue'
+import LoginView from '@/views/LoginView.vue'
+import ChargePointsView from '@/views/ChargePointsView.vue'
+import ChargePointLocationsView from '@/views/ChargePointLocationsView.vue'
+import ChargePointPortsView from '@/views/ChargePointPortsView.vue'
+import UserGroupsView from '@/views/UserGroupsView.vue'
+import UsersView from '@/views/UsersView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      redirect: '/home',
+      redirect: '/sites',
       name: 'Blank'
     },
     {
@@ -19,10 +23,35 @@ const router = createRouter({
       name: 'Dashboard',
       component: DashboardLayout,
       children: [
+        // {
+        //   path: 'home',
+        //   name: 'Home',
+        //   component: HomeView
+        // },
         {
-          path: 'home',
-          name: 'Home',
-          component: HomeView
+          path: 'sites',
+          name: 'Charge Point Locations',
+          component: ChargePointLocationsView
+        },
+        {
+          path: 'charge-points',
+          name: 'Charge Points',
+          component: ChargePointsView
+        },
+        {
+          path: 'connectors',
+          name: 'Charge Point Ports',
+          component: ChargePointPortsView
+        },
+        {
+          path: 'user-groups',
+          name: 'User Groups',
+          component: UserGroupsView
+        },
+        {
+          path: 'users',
+          name: 'Users',
+          component: UsersView
         }
       ]
     },
