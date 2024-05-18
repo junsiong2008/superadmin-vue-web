cabf8c96d8e00daae0b15b185631f1aaf5171323
Jun Siong <junsiong2599@gmail.com>
2024-05-18 13:08:22 +0800

    Added two props for InputButton: type, isLong

--- src/components/inputs/InputButton.vue
+++ src/components/inputs/InputButton.vue
@@ -1,12 +1,33 @@
 <script lang="ts" setup>
+import type { PropType } from 'vue'
+
 defineProps({
   label: {
     type: String,
     default: ''
+  },
+  type: {
+    type: String as PropType<
+      'primary' | 'disabled' | 'alert' | 'success' | 'warning' | 'blank' | 'secondary'
+    >,
+    default: 'primary'
+  },
+  isLong: {
+    type: Boolean,
+    default: false
   }
 })
 </script>
 
 <template>
-  <div class="btn btn-primary d-grid w-100">{{ label }}</div>
+  <div
+    :class="[
+      'btn',
+      'd-grid',
+      isLong ? 'w-100' : '',
+      type === 'primary' ? 'btn-primary' : type === 'secondary' ? 'btn-outline-secondary' : ''
+    ]"
+  >
+    {{ label }}
+  </div>
 </template>
